dbdb9c2458e7a0329e1b6089a61a0beabed9d616
piyushraghvani <piyush.3344@gmail.com>
2022-12-12 09:46:37 +0530

    first commit

---
+++ JOIEnergy.Tests/PricePlanComparisonTest.cs
@@ -0,0 +1,115 @@
+﻿using JOIEnergy.Controllers;
+using JOIEnergy.Domain;
+using JOIEnergy.Enums;
+using JOIEnergy.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+using Xunit.Abstractions;
+using Newtonsoft.Json.Linq;
+
+namespace JOIEnergy.Tests
+{
+    public class PricePlanComparisonTest
+    {
+        private MeterReadingService meterReadingService;
+        private PricePlanComparatorController controller;
+        private Dictionary<string, Supplier> smartMeterToPricePlanAccounts = new Dictionary<string, Supplier>();
+        private static String SMART_METER_ID = "smart-meter-id";
+
+        public PricePlanComparisonTest()
+        {
+            var readings = new Dictionary<string, List<Domain.ElectricityReading>>();
+            meterReadingService = new MeterReadingService(readings);
+            var pricePlans = new List<PricePlan>() { 
+                new PricePlan() { EnergySupplier = Supplier.DrEvilsDarkEnergy, UnitRate = 10, PeakTimeMultiplier = NoMultipliers() }, 
+                new PricePlan() { EnergySupplier = Supplier.TheGreenEco, UnitRate = 2, PeakTimeMultiplier = NoMultipliers() },
+                new PricePlan() { EnergySupplier = Supplier.PowerForEveryone, UnitRate = 1, PeakTimeMultiplier = NoMultipliers() } 
+            };
+            var pricePlanService = new PricePlanService(pricePlans, meterReadingService);
+            var accountService = new AccountService(smartMeterToPricePlanAccounts);
+            controller = new PricePlanComparatorController(pricePlanService, accountService);
+        }
+
+        [Fact]
+        public void ShouldCalculateCostForMeterReadingsForEveryPricePlan()
+        {
+            var electricityReading = new ElectricityReading() { Time = DateTime.Now.AddHours(-1), Reading = 15.0m };
+            var otherReading = new ElectricityReading() { Time = DateTime.Now, Reading = 5.0m };
+            meterReadingService.StoreReadings(SMART_METER_ID, new List<ElectricityReading>() { electricityReading, otherReading });
+
+            var result = controller.CalculatedCostForEachPricePlan(SMART_METER_ID).Value;
+
+            var actualCosts = ((JObject)result).ToObject<Dictionary<string, decimal>>();
+            Assert.Equal(3, actualCosts.Count);
+            Assert.Equal(100m, actualCosts["" + Supplier.DrEvilsDarkEnergy], 3);
+            Assert.Equal(20m, actualCosts["" + Supplier.TheGreenEco], 3);
+            Assert.Equal(10m, actualCosts["" + Supplier.PowerForEveryone], 3);
+        }
+
+        [Fact]
+        public void ShouldRecommendCheapestPricePlansNoLimitForMeterUsage()
+        {
+            meterReadingService.StoreReadings(SMART_METER_ID, new List<ElectricityReading>() {
+                new ElectricityReading() { Time = DateTime.Now.AddMinutes(-30), Reading = 35m },
+                new ElectricityReading() { Time = DateTime.Now, Reading = 3m }
+            });
+
+            object result = controller.RecommendCheapestPricePlans(SMART_METER_ID, null).Value;
+            var recommendations = ((IEnumerable<KeyValuePair<string, decimal>>)result).ToList();
+
+            Assert.Equal("" + Supplier.PowerForEveryone, recommendations[0].Key);
+            Assert.Equal("" + Supplier.TheGreenEco, recommendations[1].Key);
+            Assert.Equal("" + Supplier.DrEvilsDarkEnergy, recommendations[2].Key);
+            Assert.Equal(38m, recommendations[0].Value, 3);
+            Assert.Equal(76m, recommendations[1].Value, 3);
+            Assert.Equal(380m, recommendations[2].Value, 3);
+            Assert.Equal(3, recommendations.Count);
+        }
+
+        [Fact]
+        public void ShouldRecommendLimitedCheapestPricePlansForMeterUsage() 
+        {
+            meterReadingService.StoreReadings(SMART_METER_ID, new List<ElectricityReading>() {
+                new ElectricityReading() { Time = DateTime.Now.AddMinutes(-45), Reading = 5m },
+                new ElectricityReading() { Time = DateTime.Now, Reading = 20m }
+            });
+
+            object result = controller.RecommendCheapestPricePlans(SMART_METER_ID, 2).Value;
+            var recommendations = ((IEnumerable<KeyValuePair<string, decimal>>)result).ToList();
+
+            Assert.Equal("" + Supplier.PowerForEveryone, recommendations[0].Key);
+            Assert.Equal("" + Supplier.TheGreenEco, recommendations[1].Key);
+            Assert.Equal(16.667m, recommendations[0].Value, 3);
+            Assert.Equal(33.333m, recommendations[1].Value, 3);
+            Assert.Equal(2, recommendations.Count);
+        }
+
+        [Fact]
+        public void ShouldRecommendCheapestPricePlansMoreThanLimitAvailableForMeterUsage()
+        {
+            meterReadingService.StoreReadings(SMART_METER_ID, new List<ElectricityReading>() {
+                new ElectricityReading() { Time = DateTime.Now.AddMinutes(-30), Reading = 35m },
+                new ElectricityReading() { Time = DateTime.Now, Reading = 3m }
+            });
+
+            object result = controller.RecommendCheapestPricePlans(SMART_METER_ID, 5).Value;
+            var recommendations = ((IEnumerable<KeyValuePair<string, decimal>>)result).ToList();
+
+            Assert.Equal(3, recommendations.Count);
+        }
+
+        [Fact]
+        public void GivenNoMatchingMeterIdShouldReturnNotFound()
+        {
+            Assert.Equal(404, controller.CalculatedCostForEachPricePlan("not-found").StatusCode);
+        }
+
+        private static List<PeakTimeMultiplier> NoMultipliers()
+        {
+            return new List<PeakTimeMultiplier>();
+        }
+    }
+}
---
+++ JOIEnergy/Startup.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using JOIEnergy.Domain;
+using JOIEnergy.Enums;
+using JOIEnergy.Generator;
+using JOIEnergy.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using JOIEnergy.Middleware;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+
+namespace JOIEnergy
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var readings =
+                GenerateMeterElectricityReadings();
+
+            var pricePlans = new List<PricePlan> {
+                new PricePlan{
+                    EnergySupplier = Enums.Supplier.DrEvilsDarkEnergy,
+                    UnitRate = 10m,
+                    PeakTimeMultiplier = new List<PeakTimeMultiplier>()
+                },
+                new PricePlan{
+                    EnergySupplier = Enums.Supplier.TheGreenEco,
+                    UnitRate = 2m,
+                    PeakTimeMultiplier = new List<PeakTimeMultiplier>()
+                },
+                new PricePlan{
+                    EnergySupplier = Enums.Supplier.PowerForEveryone,
+                    UnitRate = 1m,
+                    PeakTimeMultiplier = new List<PeakTimeMultiplier>()
+                }
+            };
+
+            services.AddMvc();
+            services.AddTransient<IAccountService, AccountService>();
+            services.AddTransient<IMeterReadingService, MeterReadingService>();
+            services.AddTransient<IPricePlanService, PricePlanService>();
+            services.AddSingleton((IServiceProvider arg) => readings);
+            services.AddSingleton((IServiceProvider arg) => pricePlans);
+            services.AddSingleton((IServiceProvider arg) => SmartMeterToPricePlanAccounts);
+            services.AddMvc(options => options.EnableEndpointRouting = false);
+
+            services.AddControllers();
+
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "ToDo API",
+                    Description = "An ASP.NET Core Web API for managing ToDo items",
+                    TermsOfService = new Uri("https://example.com/terms"),
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Example Contact",
+                        Url = new Uri("https://example.com/contact")
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "Example License",
+                        Url = new Uri("https://example.com/license")
+                    }
+                });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.EnvironmentName == Environments.Development)
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(options =>
+                {
+                    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+                    options.RoutePrefix = string.Empty;
+                });
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+            }
+
+            app.UseMiddleware<ExceptionMiddleware>();
+        }
+
+        private Dictionary<string, List<ElectricityReading>> GenerateMeterElectricityReadings()
+        {
+            var readings = new Dictionary<string, List<ElectricityReading>>();
+            var generator = new ElectricityReadingGenerator();
+            var smartMeterIds = SmartMeterToPricePlanAccounts.Select(mtpp => mtpp.Key);
+
+            foreach (var smartMeterId in smartMeterIds)
+            {
+                readings.Add(smartMeterId, generator.Generate(20));
+            }
+            return readings;
+        }
+
+        public Dictionary<String, Supplier> SmartMeterToPricePlanAccounts
+        {
+            get
+            {
+                Dictionary<String, Supplier> smartMeterToPricePlanAccounts = new Dictionary<string, Supplier>();
+                smartMeterToPricePlanAccounts.Add("smart-meter-0", Supplier.DrEvilsDarkEnergy);
+                smartMeterToPricePlanAccounts.Add("smart-meter-1", Supplier.TheGreenEco);
+                smartMeterToPricePlanAccounts.Add("smart-meter-2", Supplier.DrEvilsDarkEnergy);
+                smartMeterToPricePlanAccounts.Add("smart-meter-3", Supplier.PowerForEveryone);
+                smartMeterToPricePlanAccounts.Add("smart-meter-4", Supplier.TheGreenEco);
+                return smartMeterToPricePlanAccounts;
+            }
+        }
+    }
+}

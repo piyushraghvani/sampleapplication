499b4171621759e87f3c49a2f8cc277e4324746a
piyushraghvani <piyush.3344@gmail.com>
2022-12-16 10:16:20 +0530

    updated docker file

--- JOIEnergy/Startup.cs
+++ JOIEnergy/Startup.cs
@@ -92,7 +92,7 @@ namespace JOIEnergy
                 app.UseSwagger();
                 app.UseSwaggerUI(options =>
                 {
-                    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+                    options.SwaggerEndpoint("../swagger/v1/swagger.json", "v1");
                     options.RoutePrefix = string.Empty;
                 });
             }
